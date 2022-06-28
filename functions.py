34c3d4a54f4d4c90b2047959fc2a5dcdcd167200
StephenSpoerri <108352200+StephenSpoerri@users.noreply.github.com>
2022-06-28 09:52:02 -0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,51 @@
+#add
+def addition(x,y):
+	g = x+y
+	return g
+def add():
+	x = 2
+	y = 5
+	return (addition(x,y))
+	
+print(add())
+
+
+#hi
+def greeting():
+	s = 'hello'
+	return s
+print(greeting())
+
+
+#division
+def division(b,c):
+
+	t = (b/c)
+	return t
+def divide():
+	b = 5
+	c = 10
+	return (division(b,c))
+	
+print(divide())
+
+#*
+def multiplication(o,p):
+    
+    u = (o*p)
+    return u
+def multiply ():
+    o=12
+    p=15
+    return (multiplication(o,p))
+print (multiply())
+
+#-
+def subtraction(o,p):
+    b = (o-p)
+    return b
+def subtract ():
+    o=96
+    p=58
+    return (subtraction(o,p))
+print (subtract())
